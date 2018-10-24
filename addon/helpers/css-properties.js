36b430d677f696873673bd9669834de5ee07fcd8
Jan Buschtöns <jan.buschtoens@clark.de>
2018-10-24 16:59:43 +0200

    fix: accept non-string values

--- addon/helpers/css-properties.js
+++ addon/helpers/css-properties.js
@@ -12,7 +12,7 @@ export function cssProperties([objStyles = {}], hashStyles = {}) {
 
     if (isEmpty(propertyValue)) continue;
 
-    const [singlePropertyValue] = propertyValue.split(';');
+    const [singlePropertyValue] = String(propertyValue).split(';');
 
     stringStyles += `${cssesc(propertyName)}:${cssesc(singlePropertyValue)};`;
   }
