0cad531eee820fdab3d1e97c08a90ba7ade5cef7
Jan Buschtöns <jan.buschtoens@clark.de>
2018-11-13 10:02:50 +0100

    chore(tests): fix merges

--- tests/integration/helpers/css-properties-test.js
+++ tests/integration/helpers/css-properties-test.js
@@ -7,31 +7,53 @@ moduleForComponent('css-properties', 'helper:css-properties', {
 
 // Replace this with your real tests.
 test('it creates a safe string for a single named attribute', function(assert) {
+  this.render(
+    hbs`<div data-test-width-property style="{{css-properties width='500px'}}"></div>`
+  );
 
-  this.render(hbs`<div data-test-width-property style="{{css-properties width='500px'}}"></div>`);
-
-  assert.equal(document.querySelector('[data-test-width-property]').style.width, '500px');
+  assert.equal(
+    document.querySelector('[data-test-width-property]').style.width,
+    '500px'
+  );
 });
 
 test('it should escape malicious property values', function(assert) {
-  this.set('maliciousValue', '10px; color: red;"><h2>Hello World</h2></div><div style="');
+  this.set(
+    'maliciousValue',
+    '10px; color: red;"><h2>Hello World</h2></div><div style="'
+  );
 
-  this.render(hbs`<div data-test-malicious style="{{css-properties width=maliciousValue}}"></div>`);
+  this.render(
+    hbs`<div data-test-malicious style="{{css-properties width=maliciousValue}}"></div>`
+  );
 
   const el = document.querySelector('[data-test-malicious]');
 
-  assert.equal(el.style.width, '10px', 'Passes first set value in potentially malicious values');
+  assert.equal(
+    el.style.width,
+    '10px',
+    'Passes first set value in potentially malicious values'
+  );
   assert.notEqual(el.style.color, 'red', 'Does not pass values after a ";"');
 
-  this.set('otherMaliciousValue', '10px"><h2>Hello World</h2></div><div style="');
-  this.render(hbs`<div data-test-malicious-2 style="{{css-properties width=otherMaliciousValue}}"></div>`);
+  this.set(
+    'otherMaliciousValue',
+    '10px"><h2>Hello World</h2></div><div style="'
+  );
+  this.render(
+    hbs`<div data-test-malicious-2 style="{{css-properties width=otherMaliciousValue}}"></div>`
+  );
   const el2 = document.querySelector('[data-test-malicious-2]');
 
-  assert.notEqual(el2.style.width, '10px', 'Malicious values are not made into valid values');
+  assert.notEqual(
+    el2.style.width,
+    '10px',
+    'Malicious values are not made into valid values'
+  );
 });
 
 test('it should allow object invocation', function(assert) {
-  let myStyles = {
+  const myStyles = {
     color: 'red',
     width: '20%',
     'background-image': 'url(http://placecage.com/200/200)'
@@ -39,19 +61,20 @@ test('it should allow object invocation', function(assert) {
 
   this.set('myStyles', myStyles);
 
-  this.render(hbs`<div data-test-mystyle style="{{css-properties myStyles}}"></div>`);
+  this.render(
+    hbs`<div data-test-mystyle style="{{css-properties myStyles}}"></div>`
+  );
 
-  let el = document.querySelector('[data-test-mystyle]');
+  const el = document.querySelector('[data-test-mystyle]');
 
   assert.equal(el.style.color, myStyles.color);
   assert.equal(el.style.width, myStyles.width);
 
-  /*eslint no-useless-escape: 0*/
-  assert.equal(el.style.backgroundImage, `url(\"http://placecage.com/200/200\")`);
+  assert.equal(el.style.backgroundImage, `url("http://placecage.com/200/200")`);
 });
 
 test('it should ignore empty values', function(assert) {
-  let myStyles = {
+  const myStyles = {
     color: 'red',
     width: undefined,
     'background-image': null
@@ -59,9 +82,11 @@ test('it should ignore empty values', function(assert) {
 
   this.set('myStyles', myStyles);
 
-  this.render(hbs`<div data-test-mystyle style="{{css-properties myStyles}}"></div>`);
+  this.render(
+    hbs`<div data-test-mystyle style="{{css-properties myStyles}}"></div>`
+  );
 
-  let el = this.$('[data-test-mystyle]')[0];
+  const el = document.querySelector('[data-test-mystyle]');
 
   assert.equal(el.style.color, 'red');
 
@@ -70,15 +95,17 @@ test('it should ignore empty values', function(assert) {
 });
 
 test('it should accept non-string values', function(assert) {
-  let myStyles = {
+  const myStyles = {
     'line-height': 1.5
   };
 
   this.set('myStyles', myStyles);
 
-  this.render(hbs`<div data-test-mystyle style="{{css-properties myStyles}}"></div>`);
+  this.render(
+    hbs`<div data-test-mystyle style="{{css-properties myStyles}}"></div>`
+  );
 
-  let el = this.$('[data-test-mystyle]')[0];
+  const el = document.querySelector('[data-test-mystyle]');
 
   assert.equal(el.style.lineHeight, '1.5');
 });
